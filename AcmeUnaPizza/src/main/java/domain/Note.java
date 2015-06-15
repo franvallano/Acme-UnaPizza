1f8336092413575def82fe198a353209099e4cfd
fradelval <fradelval@01362568-4560-4ab2-b0ad-5ba92d08d99f>
2015-06-15 22:42:59 +0000

    Añadidas clase, repo y converter de Customer. Completada clase de Note.

--- AcmeUnaPizza/src/main/java/domain/Note.java
+++ AcmeUnaPizza/src/main/java/domain/Note.java
@@ -3,6 +3,9 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 
 @Embeddable
@@ -18,7 +21,8 @@ public class Note {
 		}
 
 		//Getters and setter ------------------------------------------------------------------------
-		
+		@NotBlank
+		@Pattern(regexp = "^joke$|^cancelled$|^undelivered$|^other$")
 		public String getCause() {
 			return cause;
 		}
@@ -27,6 +31,7 @@ public class Note {
 			this.cause = cause;
 		}
 
+		@NotBlank
 		public String getDescription() {
 			return description;
 		}
@@ -34,7 +39,12 @@ public class Note {
 		public void setDescription(String description) {
 			this.description = description;
 		}
+
 		
+		public String toString() {
+			return "Note [cause=" + cause + ", description=" + description
+					+ "]";
+		}
 		
 		//Relationships -----------------------------------------------------------------------------
 		
