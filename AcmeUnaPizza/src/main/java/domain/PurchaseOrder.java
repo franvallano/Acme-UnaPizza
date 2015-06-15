3b2a9428b3caf29142a1ebd6ea42fee4b4213625
jesgonbel <adlebzelaznog@users.noreply.github.com>
2015-06-15 23:17:55 +0000

    Impl modelo dominio PurchaseOrder y SalesOrder

--- AcmeUnaPizza/src/main/java/domain/PurchaseOrder.java
+++ AcmeUnaPizza/src/main/java/domain/PurchaseOrder.java
@@ -4,7 +4,12 @@ import java.util.Date;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -21,7 +26,35 @@ public class PurchaseOrder extends DomainEntity{
 	}
 	
 	//Getters and setter ------------------------------------------------------------------------
+	@Pattern(regexp="[A-Z]{3}-[0-9]{8}")
+	@Column(unique=true)
+	public String getReferenceNumber() {
+		return referenceNumber;
+	}
 
+	public void setReferenceNumber(String referenceNumber) {
+		this.referenceNumber = referenceNumber;
+	}
+
+	@Min(0)
+	@Digits(integer=9, fraction=2)
+	public Double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(Double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	@Past
+	public Date getCreationMoment() {
+		return creationMoment;
+	}
+
+	public void setCreationMoment(Date creationMoment) {
+		this.creationMoment = creationMoment;
+	}
+	
 	
 	//Relationships -----------------------------------------------------------------------------
 	
--- AcmeUnaPizza/src/main/java/domain/SalesOrder.java
+++ AcmeUnaPizza/src/main/java/domain/SalesOrder.java
@@ -1,14 +1,26 @@
 package domain;
 
+import java.util.Date;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class SalesOrder extends DomainEntity{
 
 	//Attributes --------------------------------------------------------------------------------
+	private String referenceNumber, state;
+	private Note note;
+	private Date creationMoment, deliveredMoment;
+	private Double totalCost;
 	
 	
 	//Constructor -------------------------------------------------------------------------------
@@ -17,7 +29,61 @@ public class SalesOrder extends DomainEntity{
 	}
 	
 	//Getters and setter ------------------------------------------------------------------------
+	@Column(unique=true)
+	@Pattern(regexp="[A-Z]{3}-[0-9]{8}")
+	public String getReferenceNumber() {
+		return referenceNumber;
+	}
 
+	public void setReferenceNumber(String referenceNumber) {
+		this.referenceNumber = referenceNumber;
+	}
+
+	@Pattern(regexp="^open$|^cooking$|^prepared$|^onitsway$|^delivered$|^undelivered$")
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Valid
+	public Note getNote() {
+		return note;
+	}
+
+	public void setNote(Note note) {
+		this.note = note;
+	}
+
+	@Past
+	public Date getCreationMoment() {
+		return creationMoment;
+	}
+
+	public void setCreationMoment(Date creationMoment) {
+		this.creationMoment = creationMoment;
+	}
+
+	@Past
+	public Date getDeliveredMoment() {
+		return deliveredMoment;
+	}
+
+	public void setDeliveredMoment(Date deliveredMoment) {
+		this.deliveredMoment = deliveredMoment;
+	}
+
+	@Min(0)
+	@Digits(integer=9, fraction=2)
+	public Double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(Double totalCost) {
+		this.totalCost = totalCost;
+	}
 	
 	//Relationships -----------------------------------------------------------------------------
 	
