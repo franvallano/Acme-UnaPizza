4afed3b324ab62fad95505e2569b6e89c72d5fce
jaiguecub <jaiguecub@01362568-4560-4ab2-b0ad-5ba92d08d99f>
2015-07-10 10:53:43 +0000

    Generador patrones en purchaseorder

---
+++ AcmeUnaPizza/src/main/java/utilities/PatternGenerator.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+public class PatternGenerator {
+	public static String purchaseOrderReferenceNumber() {
+		String referenceNumber = "";
+		
+		referenceNumber = RandomStringUtils.randomAlphabetic(3).toUpperCase() + "-";
+		
+		for(int i=0;i<8;i++) 
+			referenceNumber += (int)Math.floor(Math.random()*(10));
+		
+		return referenceNumber;
+	}
+}
