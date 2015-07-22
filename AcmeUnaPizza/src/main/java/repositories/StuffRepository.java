e95f4bd0a39cac629217ead3f0de5b42105923d6
jesgonbel <adlebzelaznog@users.noreply.github.com>
2015-07-22 19:36:05 +0000

    Añadida funcionalidad para encontrar todos los elementos que pueden ser reparados en un taller especifico.

--- AcmeUnaPizza/src/main/java/repositories/StuffRepository.java
+++ AcmeUnaPizza/src/main/java/repositories/StuffRepository.java
@@ -14,4 +14,7 @@ public interface StuffRepository extends JpaRepository<Stuff, Integer>{
 
 	@Query("select s from Stuff s where s.repairs.size = (select max(st.repairs.size) from Stuff st)")
 	Collection<Stuff> findStuffMoreRepaired();
+	
+	@Query("select s from Stuff s where s.status='MALFUNCTION' and s.workShop.id=?1")
+	Collection<Stuff> findAllMalfunctionStuffByWorkshopId(int id);
 }
